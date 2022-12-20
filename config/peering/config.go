cffc4366abe9a969c4cb215670e4570e04927841
antonum <aumnikov@gmail.com>
2022-12-20 17:05:20 -0500

    add peering resource #2

--- config/peering/config.go
+++ config/peering/config.go
@@ -7,6 +7,6 @@ func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("rediscloud_subscription_peering", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
 		// this resource, which would be "github"
-		r.ShortGroup = "database"
+		r.ShortGroup = "peering"
 	})
 }
